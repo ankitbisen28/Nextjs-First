7d5485861004f678233ac97c85bb366d45550218
Ankit Bisen <ankitbisen288@gmail.com>
2023-10-12 17:19:16 +0530

    added middleware and me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,16 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextResponse, NextRequest } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password");
+    return NextResponse.json({ message: "User Found", data: user });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,56 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = React.useState("nothing");
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      ProfilePage
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile Page</p>
+      <hr />
+      <h2 className="p-3 rounded bg-green-400">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-purple-900 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Get UserDetails
+      </button>
     </div>
   );
 }
